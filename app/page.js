d4a8d298b0075088e25083f4105fb5c82ef4cfc1
Graham Michael Rowe <gmrowe@Grahams-MacBook-Pro.local>
2023-11-06 11:05:15 -0500

    add next pub

--- app/page.js
+++ app/page.js
@@ -4,11 +4,14 @@ import styles from './page.module.css'
 export default function Home() {
   let TEST = process.env.TEST
   let NEXT_TEST = process.env.NEXT_TEST
+  let NEXT_PUBLIC_TEST = process.NEXT_PUBLIC_TEST
   return (
     <main className={styles.main}>
       {TEST || 'NO TEST'}
       <br/>
       {NEXT_TEST || 'NO NEXT TEST'}
+      <br />
+      {NEXT_PUBLIC_TEST || 'NO NEXT_PUBLIC_TEST'}
     </main>
   )
 }
